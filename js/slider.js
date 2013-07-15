ef7f8c80acde144ba412527d0d01e42b785514d7
Taylor Young <taylor@x-team.com>
2013-07-15 13:17:54 -0500

    20130715:13:17

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,1 @@
-require.config({baseUrl:"js",paths:{cycle:"../bower/cycle/index"},shim:{cycle:{deps:["../bower/jquery/index"]}}}),define(["require","common","cycle"],function(e){e("common"),e("cycle")});
+require.config({baseUrl:"js",paths:{cycle:"../bower/cycle/index"},shim:{cycle:{deps:["../bower/jquery/index"]}}}),define(["require","cycle"],function(e){return function(){e("cycle"),$(".cycle-slideshow").on("cycle-before",function(e,t,n,r,i){console.log(e,n)})}});
